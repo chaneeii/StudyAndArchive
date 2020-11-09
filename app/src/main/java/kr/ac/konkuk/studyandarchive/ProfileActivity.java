eba7418ae98f9bd955619674608cf7fc502cb133
Chanhee <chan420420@gmail.com>
2020-11-10 00:56:06 +0900

    Login Function

--- app/src/main/java/kr/ac/konkuk/studyandarchive/ProfileActivity.java
+++ app/src/main/java/kr/ac/konkuk/studyandarchive/ProfileActivity.java
@@ -1,10 +1,14 @@
 package kr.ac.konkuk.studyandarchive;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,8 +16,10 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity {
 
     //firebase auth
-
     FirebaseAuth firebaseAuth;
+
+    //views
+    TextView mProfileTv;
 
 
     @Override
@@ -29,6 +35,9 @@ public class ProfileActivity extends AppCompatActivity {
         //init
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //init views
+        mProfileTv = findViewById(R.id.profileTV);
+
 
 
     }
@@ -42,6 +51,8 @@ public class ProfileActivity extends AppCompatActivity {
         if(user !=null){
             //user is signed in stay here
             // 만약 로그인 되어있다면 홈보여주기
+            //set email of logged in user 로그인된 사용자의 이메일 세팅
+            mProfileTv.setText(user.getEmail());
 
         }else{
             //user not signed in, go to main activity
@@ -59,6 +70,29 @@ public class ProfileActivity extends AppCompatActivity {
         super.onStart();
     }
 
+
+    /*inflate option menu*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //inflating menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /*Handle menu item clicks*/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //get item id
+        int id = item.getItemId();
+        if(id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus(); //로그인안되면 메인감감
+       }
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 
