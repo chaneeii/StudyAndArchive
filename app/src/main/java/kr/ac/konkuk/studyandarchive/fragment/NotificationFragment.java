eb7f2ee9b92bb168f9cc79557d07abf5c3fc6330
Chanhee <chan420420@gmail.com>
2020-11-19 17:29:07 +0900

    NotificationFragment

---
+++ app/src/main/java/kr/ac/konkuk/studyandarchive/fragment/NotificationFragment.java
@@ -0,0 +1,36 @@
+package kr.ac.konkuk.studyandarchive.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.ac.konkuk.studyandarchive.R;
+
+
+public class NotificationFragment extends Fragment {
+
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+        return view;
+    }
+}
